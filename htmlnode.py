2c3d193ec90f5bf0b59fbf4b011e41fe5a9acf78
SuperflyCasanova <livioq@gmail.com>
2024-03-14 12:33:11 +0100

    created htmlnode.py

---
+++ htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        attributes = []
+        for key in self.props:
+            value = self.props[key]
+            attributes.append(f" {key}=\"{value}\"")
+        return ' '.join(attributes)
+
+    def __repr__(self):
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})
+        
