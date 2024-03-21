ba42842b00e466bb33e93cb2c8472ea45b2e4826
SuperflyCasanova <livioq@gmail.com>
2024-03-21 15:26:46 +0100

    minor bug fix

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -12,12 +12,12 @@ from textnode import (
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type != text_type_text:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
             continue
         split_nodes = []
-        sections = node.text.split(delimiter)
+        sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
             raise ValueError("Delimiter not properly closed")
         for i in range(len(sections)):
